7ef96850af69f3b571ea246a7f2e56c2971a7023
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-04-18 15:31:57 -0600

    setting up my inquirer with .prompt and type input and putting my array of questions in there, got first question set up

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = ["What is the title of your project?", "What is the description of your project?", "What are the installation instructions for your project?", "What are the usage instructions for your project?", "What are the contribution guidelines for your project?", "What are the test instructions for your project?", "What license would you like to use for your project?", "What is your GitHub username?", "What is your email address?"];
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: questions[0],
+            name: 'title',
+        },
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    
+function createReadMe('README.md', data) {
+
 }
 
 // TODO: Create a function to initialize app
