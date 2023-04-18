07ba3d15bbeaabfdaaca9c6930ecccc2eab18560
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-04-18 15:21:30 -0600

    added the questions i wanted to create the README file in the given const questions array

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is the title of your project?", "What is the description of your project?", "What are the installation instructions for your project?", "What are the usage instructions for your project?", "What are the contribution guidelines for your project?", "What are the test instructions for your project?", "What license would you like to use for your project?", "What is your GitHub username?", "What is your email address?"];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {}
