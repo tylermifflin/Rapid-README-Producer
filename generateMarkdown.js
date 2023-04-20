868157d5e365544b9b57e952ab7106c3228c71ed
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-04-20 15:23:16 -0600

    added renderLicenseSection function to display which license chosen on the README file

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -33,7 +33,19 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+    if (license === 'MIT') {
+        return 'Licensed under the MIT license.';
+    } else if (license === 'Apache 2.0') {
+        return 'Licensed under the Apache 2.0 license.';
+    } else if (license === 'GPL 3.0') {
+        return 'Licensed under the GPL 3.0 license.';
+    } else if (license === 'BSD 3') {
+        return 'Licensed under the BSD 3 license.';
+    } else {
+        return '';
+    }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
