ce9cf860d52a30a3f5b55b9d9eb296f3c149c134
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-04-20 15:21:13 -0600

    added funtions to the generateMarkdown file to return the license badge and link to license if it is chosen

---
+++ generateMarkdown.js
@@ -0,0 +1,45 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+    if (license === 'MIT') {
+        return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    } else if (license === 'Apache 2.0') {
+        return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+    } else if (license === 'GPL 3.0') {
+        return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+    } else if (license === 'BSD 3') {
+        return '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+    } else {
+        return '';
+    }
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+    if (license === 'MIT') {
+        return 'https://opensource.org/licenses/MIT';
+    } else if (license === 'Apache 2.0') {
+        return 'https://opensource.org/licenses/Apache-2.0';
+    } else if (license === 'GPL 3.0') {
+        return 'https://www.gnu.org/licenses/gpl-3.0';
+    } else if (license === 'BSD 3') {
+        return 'https://opensource.org/licenses/BSD-3-Clause';
+    } else {
+        return '';
+    }
+
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+`;
+}
+
+module.exports = generateMarkdown;
